04422361b90f104a542ad64e68273979352a7222
Jan-Lukas Else <mail@jlelse.de>
2020-03-15 19:48:13 +0100

    MailyGo

--- admin/src/models/submissionModel.ts
+++
@@ -1,8 +0,0 @@
-export interface SubmissionModel {
-    id: string;
-    time: string;
-    formName: string;
-    replyTo: string;
-    text: string;
-    response: string;
-}
--- admin/vue.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    publicPath: '/admin/',
-    runtimeCompiler: true
-};
